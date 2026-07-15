9e475f6da7076eb422b5a40e54aa94f55ab33094
Lucas <lrassbach@github.com>
2026-07-15 14:12:27 -0700

    implement initial test of tof multi sensor bus

--- test/test_lidar/test_lidar.py
+++ test/test_lidar/test_lidar.py
@@ -2,17 +2,18 @@ from rplidar import RPLidar
 import time
 PORT = '/dev/ttyUSB0'   # Change if needed
 
-lidar = RPLidar(PORT)
+lidar = RPLidar(PORT, timeout=3.0)
 time.sleep(0.5)
 print("RPLidar A2 connected. Starting scan...")
 
 try:
+    print("start motor")
     lidar.start_motor()
-
+    time.sleep(1.0)
     for scan in lidar.iter_scans():
         print(scan)   # list of (quality, angle, distance)
-except Exception:
-    print("Stopping.")
+except Exception as e:
+    print("Stopping: " + e)
 finally:
     lidar.stop()
     lidar.stop_motor()
--- test/test_tof/test_tof.ino
+++ test/test_tof/test_tof.ino
@@ -1,33 +1,87 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 
-VL53L0X sensor;
+const uint8_t ADDR_SENSOR1 = 0x30;
+const uint8_t ADDR_SENSOR2 = 0x31;
+const uint8_t ADDR_SENSOR3 = 0x32;
+
+VL53L0X sensor1; // left -- BLUE
+VL53L0X* sensor1_ptr = &sensor1;
+VL53L0X sensor2; // center -- YELLOW
+VL53L0X* sensor2_ptr = &sensor2;
+VL53L0X sensor3; // right -- GREEN
+VL53L0X* sensor3_ptr = &sensor3;
+
+const int xshut1 = 23;
+const int xshut2 = 22;
+const int xshut3 = 9;
+
 int failed = 0;
-void setup() {
-  Serial.begin(57600);
-  Wire.begin();
 
-  Serial.println("Starting VL53L0X...");
-
-  if (!sensor.init()) {
-    Serial.println("Failed to detect and initialize sensor!");
+int start_sensor(VL53L0X* sensor, const int xshut, const uint8_t addr){
+  
+  pinMode(xshut, OUTPUT);
+  digitalWrite(xshut, LOW);
+  delay(10);
+  // wake
+  digitalWrite(xshut, HIGH);
+  delay(10);
+  // initialize
+  sensor->setTimeout(500);
+  if(!sensor->init()){
+    return 1;
   }
+  sensor->setAddress(addr);
+  sensor->startContinuous();
 
-  sensor.setTimeout(500);
-  sensor.startContinuous();
+  return 0;
 }
 
-void loop() {
+void setup() {
+  Serial.begin(57600);
+  
+  delay(2);
+  Serial.println("Starting VL53L0X...");
+  Wire.begin();
 
-  uint16_t distance = sensor.readRangeContinuousMillimeters();
+  int rc = 0;
 
-  if (sensor.timeoutOccurred()) {
+  rc += start_sensor(sensor1_ptr, xshut1, ADDR_SENSOR1);
+  rc += start_sensor(sensor2_ptr, xshut1, ADDR_SENSOR2);
+  rc += start_sensor(sensor3_ptr, xshut1, ADDR_SENSOR3);
+  if(rc != 0){
+    Serial.println("Error in sensor initialization");
+    exit(1);
+  }
+
+  delay(1);
+  Serial.println("All sensors started OK");
+
+}
+
+int read_sensor(VL53L0X* sensor){
+  uint16_t distance = sensor->readRangeContinuousMillimeters();
+
+  if (sensor->timeoutOccurred()) {
     Serial.println("Timeout!");
+    return 1;
   } else {
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.println(" mm");
+    return 0;
   }
 
+}
+
+void loop() {
+  Serial.println("Left Sensor (1)");
+  read_sensor(&sensor1);
+  delay(100);
+  Serial.println("middle Sensor (2)");
+  read_sensor(&sensor2);
+  delay(100);
+  Serial.println("right Sensor (3)");
+  read_sensor(&sensor3);
   delay(100);
 }
